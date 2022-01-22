cd4b758a49cfcb749cd9af7c6825378cacb53314
Akshita Jain <akshita.jain@baruchmail.cuny.edu>
2022-01-23 00:18:51 +0530

    commit

---
+++ code/file_paths.py
@@ -0,0 +1,4 @@
+main_file_path = "D:/Documents/College docs/Sem III/Mini project/data/NIFTY50_all.csv"
+modified_file_path = "D:/Documents/College docs/Sem III/Mini project/data/NIFTY50_all_modified.csv"
+stock_metadata_path = 'D:/Documents/College docs/Sem III/Mini project/data/stock_metadata.csv'
+finance_minister_path = 'D:/Documents/College docs/Sem III/Mini project/data/finance_minister_data.csv'
